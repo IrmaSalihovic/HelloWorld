6d3f5a8a20255dede1500c645049daf41c31ac52
IrmaSalihovic <irma9343@gmail.com>
2020-02-19 21:18:23 +0100

    First commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,20 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+
+System.out.println("Hello World");
+System.out.println(" Testing");
+		
+System.out.print("Hello World");
+System.out.print(" app");
+System.out.println(" Testing");
+
+System.out.println("Testing app with Selenium.");
+System.out.println("Save with ctr+s");
+System.out.println(1234567);
+
+	}
+
+}
